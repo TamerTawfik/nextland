258162f0e0cf2e268c816b8d5e24c15dfd354e03
Tamer Tawfik <tamertawfiklxr@gmail.com>
2022-11-07 22:17:50 +0200

    Added: split with iage  CTA section

---
+++ components/sections/CTA/SplitWithImage/SplitWithImage.style.js
@@ -0,0 +1,15 @@
+// twin-macro
+import tw from 'twin.macro';
+
+export const BackgroundContainer = tw.div`relative bg-gray-900`;
+export const LeftCol = tw.div`relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2`;
+export const Image = tw.img`w-full h-full object-cover`;
+export const BgGradient = tw.div`absolute inset-0 bg-gradient-to-r from-teal-500 to-cyan-600 mix-blend-multiply`;
+export const RightCol = tw.div`relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32`;
+export const TextContainer = tw.div`md:ml-auto md:w-1/2 md:pl-10`;
+export const Heading = tw.h2`text-base font-semibold uppercase tracking-wider text-gray-300`;
+export const SubHeading = tw.p`mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl`;
+export const Text = tw.p`mt-3 text-lg text-gray-300`;
+export const LinkWrap = tw.div`mt-8`;
+export const SecondaryLinkWrap = tw.div`inline-flex rounded-md shadow`;
+export const SecondaryLink = tw.a`inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50`;
---
+++ components/sections/CTA/SplitWithImage/index.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+// twin-macro
+import { BackgroundContainer, LeftCol, Image, BgGradient, RightCol, Heading, SubHeading, LinkWrap, SecondaryLink, SecondaryLinkWrap, TextContainer, Text } from "./SplitWithImage.style";
+// react-icons
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+
+export default function SplitWithImage({
+    imageUrl = "https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100",
+    imageAlt = "contact image",
+    heading = "Award winning support",
+    subHeading = "We’re here to help",
+    text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
+    scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum
+    tincidunt duis.`,
+    secondaryLinkText = "Visit the help center",
+    secondaryLinkUrl = "http://google.com",
+}) {
+    return (
+        <BackgroundContainer>
+            <LeftCol>
+                <Image
+                    src={imageUrl}
+                    alt={imageAlt}
+                />
+                <BgGradient
+                    aria-hidden="true"
+                />
+            </LeftCol>
+            <RightCol>
+                <TextContainer>
+                    <Heading>{heading}</Heading>
+                    <SubHeading>{subHeading}</SubHeading>
+                    <Text>
+                        {text}
+                    </Text>
+                    <LinkWrap>
+                        <SecondaryLinkWrap>
+                            <Link href={secondaryLinkUrl} passHref>
+                                <SecondaryLink>
+                                    {secondaryLinkText}
+                                    <FaExternalLinkAlt className="-mr-1 ml-3 h-5 w-5 text-gray-300" />
+                                </SecondaryLink>
+                            </Link>
+                        </SecondaryLinkWrap>
+                    </LinkWrap>
+                </TextContainer>
+            </RightCol>
+        </BackgroundContainer>
+    )
+}

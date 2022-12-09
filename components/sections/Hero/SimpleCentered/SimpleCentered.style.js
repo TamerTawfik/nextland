50c4ede5644b07b4a9107429a834023fdc626429
Tamer Tawfik <tamertawfiklxr@gmail.com>
2022-12-09 11:16:19 +0200

    Refactored: Hero section

--- components/sections/Hero/SimpleCentered/SimpleCentered.style.js
+++ components/sections/Hero/SimpleCentered/SimpleCentered.style.js
@@ -4,14 +4,10 @@ export const Container = tw.div`isolate bg-transparent`;
 
 export const HeroContainer = styled.div`
     ${tw`mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40`}
-    .ctaWrapper {
+    .wrapper {
         ${tw`hidden sm:mb-4 sm:flex sm:justify-center`}
     }
-    .cta {
-        ${tw`relative overflow-hidden rounded-full py-1.5 px-4 text-sm font-thin leading-6 ring-1 ring-accents_2  bg-accents_1 text-accents_8`}
-        a {${tw`font-thin text-success hover:text-successLight`}}
-    }
-    .slogan {
+    .gpt {
         ${tw` text-xl uppercase font-black bg-clip-text text-transparent bg-gradient-to-tr from-foreground to-accents_6 tracking-tight sm:text-3xl `}
       }
     .heading {
@@ -24,6 +20,6 @@ export const HeroContainer = styled.div`
         ${tw`inline-block rounded-md bg-foreground hover:bg-background px-10 py-1.5 text-base font-medium leading-7 text-background hover:text-foreground shadow-inner ring-1 ring-foreground`}
     }
     .liveDemo {
-        ${tw`inline-block rounded-md border border-r-success border-b-success border-l-cyan border-t-cyan bg-background hover:bg-gradient-to-r from-success via-success to-cyan  px-10 py-1.5 text-base font-medium leading-7 text-foreground  ring-1 ring-success`}
+        ${tw`inline-block rounded-md border border-r-success border-b-success border-l-cyan border-t-cyan bg-background hover:bg-gradient-to-r from-success via-success to-cyan  px-10 py-1.5 text-base font-medium leading-7 text-foreground hover:text-background  ring-1 ring-success`}
     }
 `;
--- components/sections/Hero/SimpleCentered/index.js
+++ components/sections/Hero/SimpleCentered/index.js
@@ -1,5 +1,4 @@
-import { useEffect, useRef } from "react";
-import { animate, motion } from "framer-motion";
+import Link from 'next/link'
 import tw from "twin.macro";
 import Navbar from "../../../common/Navbar/CenteredNavbar";
 import { Container, HeroContainer } from "./simpleCentered.style";
@@ -10,103 +9,36 @@ export default function SimpleCentered() {
 
     return (
         <Container>
-            {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-                <svg
-                    className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-                    viewBox="0 0 1155 678"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-                        fillOpacity=".3"
-                        d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                    />
-                    <defs>
-                        <linearGradient
-                            id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-                            x1="1155.49"
-                            x2="-78.208"
-                            y1=".177"
-                            y2="474.645"
-                            gradientUnits="userSpaceOnUse"
-                        >
-                            <stop stopColor="#9089FC" />
-                            <stop offset={1} stopColor="#FF80B5" />
-                        </linearGradient>
-                    </defs>
-                </svg>
-            </div> */}
             <Navbar />
             <div tw="relative px-6 lg:px-8">
                 <HeroContainer className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via-black to-black">
                     <div>
-                        <div className="ctaWrapper">
-                            <p className="slogan">i-max</p>
-                            {/* <div className="cta">
-                            <span className="text-xs bg-successDark rounded-full text-white px-2 py-1 mr-3">News</span> 
-                                <span>
-                                    Announcing our next round of funding.{' '}
-                                    <a href="#">
-                                        <span tw="absolute inset-0" aria-hidden="true" />
-                                            Read more <span aria-hidden="true">&rarr;</span>
-                                    </a>
-                                </span>
-                            </div> */}
+                        <div className="wrapper">
+                            <p className="gpt">GPT-3</p>
                         </div>
                         <div>
                             <h1 className="heading">
-                            AI & Machine Learning Development
-                                </h1>
+                                AI & Machine Learning Development
+                            </h1>
                             <p>
-                            We engineer practical data-driven algorithms to implement machine learning solutions for startups by separating the AI hype from computational realities.
-                                </p>
+                                We engineer practical data-driven algorithms to implement machine learning solutions for startups by separating the AI hype from computational realities.
+                            </p>
                             <div tw="mt-8 flex gap-x-4 sm:justify-center">
-                                <a
-                                    href="#"
-                                    className="getStarted"
-                                >
-                                    Get started
-                                       
+                                <Link href='#' passHref legacyBehavior>
+                                    <a className="getStarted">
+                                        Get started
                                 </a>
-                                <a
-                                    href="#"
-                                    className="liveDemo"
-                                >
-                                    Live demo
+                                </Link>
+                                <Link href='#' passHref legacyBehavior>
+                                    <a className="liveDemo">
+                                        Live demo
                                         <span tw="ml-1" aria-hidden="true">
-                                        &rarr;
+                                            &rarr;
                                         </span>
-                                </a>
+                                    </a>
+                                </Link>
                             </div>
                         </div>
-                        {/* <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                            <svg
-                                className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-                                viewBox="0 0 1155 678"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-                                    fillOpacity=".3"
-                                    d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                                />
-                                <defs>
-                                    <linearGradient
-                                        id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                                        x1="1155.49"
-                                        x2="-78.208"
-                                        y1=".177"
-                                        y2="474.645"
-                                        gradientUnits="userSpaceOnUse"
-                                    >
-                                        <stop stopColor="#9089FC" />
-                                        <stop offset={1} stopColor="#FF80B5" />
-                                    </linearGradient>
-                                </defs>
-                            </svg>
-                        </div> */}
                     </div>
                 </HeroContainer>
             </div>
